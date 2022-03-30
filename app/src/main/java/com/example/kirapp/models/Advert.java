b3277125f0056a73acc0b73e9e3a95853053e49d
İsmail Yarar <ismailyarar4@gmail.com>
2022-03-30 15:38:54 +0300

    third commit

---
+++ app/src/main/java/com/example/kirapp/models/Advert.java
@@ -0,0 +1,13 @@
+package com.example.kirapp.models;
+
+import java.time.LocalDate;
+
+public class Advert {
+    private String id;
+    private String name;
+    private LocalDate publishDate;
+    private String description;
+    private double price;
+    private boolean status;
+    //Customer ile bağlantısı yapılacak
+}
---
+++ app/src/main/java/com/example/kirapp/models/Customer.java
@@ -0,0 +1,96 @@
+package com.example.kirapp.models;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Customer {
+    private String id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private String identityNumber;
+    private LocalDate birthDate;
+    //Advertle bağlantısı yapılacak
+    //BURAYA FOTO ALMA GELECEK
+
+    public Customer(String id, String firstname, String lastname, String email,
+                    String password, String phoneNumber, String identityNumber, LocalDate birthDate) {
+        this.id = id;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.identityNumber = identityNumber;
+        this.birthDate = birthDate;
+    }
+
+    public Customer() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getIdentityNumber() {
+        return identityNumber;
+    }
+
+    public void setIdentityNumber(String identityNumber) {
+        this.identityNumber = identityNumber;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
