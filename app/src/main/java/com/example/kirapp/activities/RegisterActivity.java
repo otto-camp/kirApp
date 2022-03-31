816ea27ccacdde390dc497cfd06fb06493c6efeb
İsmail Yarar <ismailyarar4@gmail.com>
2022-03-31 10:59:04 +0300

    register page added! bottom navbar on the way

---
+++ app/src/main/java/com/example/kirapp/activities/RegisterActivity.java
@@ -0,0 +1,19 @@
+package com.example.kirapp.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.kirapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_page);
+
+    }
+}
