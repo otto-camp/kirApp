34c91a5034ad9f9c217c46d14d05390ee2e4a7b9
otto-camp <ismailyarar4@gmail.com>
2022-04-01 16:08:20 +0300

    3 fragment added bottom navbar fixed

--- app/src/main/java/com/example/kirapp/MainActivity.java
+++ app/src/main/java/com/example/kirapp/MainActivity.java
@@ -1,14 +1,50 @@
 package com.example.kirapp;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.kirapp.databinding.ActivityMainBinding;
+import com.example.kirapp.fragments.MainPageFragment;
+import com.example.kirapp.fragments.ProfileFragment;
+import com.example.kirapp.fragments.SearchFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding mainBinding;
+
+    @SuppressLint("NonConstantResourceId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mainBinding.getRoot());
+        replaceFragment(new MainPageFragment());
+
+        mainBinding.bottomNavigationView.setOnClickListener(item -> {
+            switch (item.getId()) {
+                case R.id.main_page:
+                    replaceFragment(new MainPageFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+                case R.id.search:
+                    replaceFragment(new SearchFragment());
+                    break;
+            }
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 }
