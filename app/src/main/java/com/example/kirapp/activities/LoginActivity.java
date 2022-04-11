24a7592984b6b88c96e82fcf1fa25e270139bcb6
İsmail Yarar <ismailyarar4@gmail.com>
2022-04-11 18:49:36 +0300

    ....

--- app/src/main/java/com/example/kirapp/activities/LoginActivity.java
+++ app/src/main/java/com/example/kirapp/activities/LoginActivity.java
@@ -1,15 +1,15 @@
 package com.example.kirapp.activities;
 
-import android.os.Bundle;
-
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 import com.example.kirapp.R;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
--- app/src/main/java/com/example/kirapp/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/kirapp/fragments/ProfileFragment.java
@@ -11,11 +11,32 @@ import android.view.ViewGroup;
 import com.example.kirapp.R;
 
 public class ProfileFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
 
+    private String mParam1;
+    private String mParam2;
+
+    public ProfileFragment(){
+
+    }
+
+    public static ProfileFragment newInstance(String param1, String param2) {
+        ProfileFragment fragment = new ProfileFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
     }
 
     @Override
