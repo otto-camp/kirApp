0ba99926c34ba6b15dc81aa65178f7d7f6fbab0c
İsmail Yarar <ismailyarar4@gmail.com>
2022-05-04 20:50:01 +0300

    a

--- app/src/main/java/com/example/kirapp/activities/MainActivity.java
+++ app/src/main/java/com/example/kirapp/activities/MainActivity.java
@@ -10,11 +10,15 @@ import com.example.kirapp.fragments.AddAdvertFragment;
 import com.example.kirapp.fragments.MainPageFragment;
 import com.example.kirapp.fragments.MessageFragment;
 import com.example.kirapp.fragments.ProfileFragment;
+import com.example.kirapp.fragments.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private Fragment fragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -22,30 +26,35 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_advert_btn);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.main_frame_layout, new MainPageFragment()).commit();
+
         bottomNavigationView.setSelectedItemId(R.id.mainPageFragment);
         bottomNavigationView.setOnItemSelectedListener(item -> {
-            Fragment fragment = null;
-            if (item.getItemId() == R.id.mainPageFragment) {
-                fragment = new MainPageFragment();
-            }
-            if (item.getItemId() == R.id.profileFragment) {
-                fragment = new ProfileFragment();
-            }
-            if (item.getItemId() == R.id.messageFragment) {
-                fragment = new MessageFragment();
-            }
-            if (item.getItemId() == R.id.addAdvertFragment) {
-                fragment = new AddAdvertFragment();
-            }
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.main_frame_layout, Objects.requireNonNull(fragment)).commit();
-            return true;
-        });
-    }
+                    if (item.getItemId() == R.id.mainPageFragment) {
+                        fragment = new MainPageFragment();
+                    }
+                    if (item.getItemId() == R.id.searchFragment) {
+                        fragment = new SearchFragment();
+                    }
+                    if (item.getItemId() == R.id.profileFragment) {
+                        fragment = new ProfileFragment();
+                    }
+                    if (item.getItemId() == R.id.messageFragment) {
+                        fragment = new MessageFragment();
+                    }
 
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_frame_layout, Objects.requireNonNull(fragment)).commit();
+                    return true;
+                }
+        );
+        floatingActionButton.setOnClickListener(view -> fragment = new AddAdvertFragment());
+
+
+    }
 }
 
