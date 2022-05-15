4d6a96ca549029ced6cabb9515f9f56f26e8fc01
İsmail Yarar <ismailyarar4@gmail.com>
2022-05-15 21:38:06 +0300

    profil fragment eklentileri

--- app/src/main/java/com/example/kirapp/activities/MainActivity.java
+++ app/src/main/java/com/example/kirapp/activities/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.kirapp.activities;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.example.kirapp.R;
+import com.example.kirapp.fragments.AddAdvertFragment;
+import com.example.kirapp.fragments.MainPageFragment;
+import com.example.kirapp.fragments.MessageFragment;
+import com.example.kirapp.fragments.ProfileFragment;
+import com.example.kirapp.fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -13,7 +22,39 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startActivity(new Intent(this, AdminDashboardActivity.class));
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        FloatingActionButton floatingActionButton = findViewById(R.id.add_advert_btn);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_frame_layout, new MainPageFragment()).commit();
+
+        bottomNavigationView.setSelectedItemId(R.id.mainPageFragment);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+                    Fragment fragment = null;
+                    if (item.getItemId() == R.id.mainPageFragment) {
+                        fragment = new MainPageFragment();
+                    }
+                    if (item.getItemId() == R.id.searchFragment) {
+                        fragment = new SearchFragment();
+                    }
+                    if (item.getItemId() == R.id.profileFragment) {
+                        fragment = new ProfileFragment();
+                    }
+                    if (item.getItemId() == R.id.messageFragment) {
+                        fragment = new MessageFragment();
+                    }
+
+                    getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.main_frame_layout, Objects.requireNonNull(fragment)).commit();
+                    return true;
+                }
+        );
+        floatingActionButton.setOnClickListener(view -> getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.main_frame_layout, new AddAdvertFragment())
+                .commit());
+
 
     }
 }
