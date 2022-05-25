7772dc14a4d80a9a1f62aed803aeecfea1668bd2
İsmail Yarar <ismailyarar4@gmail.com>
2022-05-25 13:24:50 +0300

    search ve message kısmının silinmesi ve wp eklenmesi

--- app/src/main/java/com/example/kirapp/fragments/SearchFragment.java
+++ app/src/main/java/com/example/kirapp/fragments/SearchFragment.java
@@ -1,23 +1,22 @@
 package com.example.kirapp.fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.widget.SearchView;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.kirapp.R;
-import com.example.kirapp.adapters.SearchAdvertAdapter;
-import com.example.kirapp.models.Advert;
-import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.example.kirapp.adapters.SearchCustomerAdapter;
+import com.example.kirapp.models.Customer;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,58 +24,73 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 public class SearchFragment extends Fragment {
-    private final DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("adverts");
-    private final ArrayList<Advert> adverts = new ArrayList<>();
-    private final Map<String, Advert> map = new HashMap<>();
-    private Advert advert = new Advert();
-    private RecyclerView recyclerView;
-    private SearchAdvertAdapter advertAdapter;
-    private FirebaseRecyclerOptions<Advert> options;
+    private DatabaseReference ref;
+    private EditText tSearch;
+    private ListView listView;
+    private ArrayList<Customer> customers;
+    private SearchCustomerAdapter adapter;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ref = FirebaseDatabase.getInstance().getReference("customers");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
-        recyclerView = view.findViewById(R.id.search_advert_list);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        listView = view.findViewById(R.id.search_list_view);
+        tSearch = view.findViewById(R.id.search_text);
+        customers = new ArrayList<>();
 
-        options = new FirebaseRecyclerOptions.Builder<Advert>()
-                .setQuery(FirebaseDatabase.getInstance().getReference("adverts"), Advert.class)
-                .build();
-
-        getAdverts(recyclerView);
+        loadUser();
 
         return view;
     }
 
-
-
-
-
-    private void getAdverts(RecyclerView recyclerView) {
-        databaseReference.addValueEventListener(new ValueEventListener() {
+    private void loadUser() {
+        ref.orderByKey().limitToLast(20).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                adverts.clear();
-                for (DataSnapshot ds : snapshot.getChildren()) {
-                    advertAdapter = new SearchAdvertAdapter(options, adverts, snapshot.getKey(), getContext());
-                    for (DataSnapshot s : ds.getChildren()) {
-                        advert = s.getValue(Advert.class);
-                        map.put(s.getKey(), advert);
+                if (snapshot.exists()) {
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        Customer customer = ds.getValue(Customer.class);
+                        customers.add(customer);
                     }
+                    adapter = new SearchCustomerAdapter(requireContext(), R.layout.search_customer_item, customers);
+                    listView.setAdapter(adapter);
+                    search();
+                } else {
+                    Toast.makeText(getContext(), "No Data", Toast.LENGTH_SHORT).show();
                 }
-                adverts.addAll(map.values());
-                recyclerView.setAdapter(advertAdapter);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-
+                Toast.makeText(getContext(), "Error", Toast.LENGTH_SHORT).show();
             }
         });
     }
+
+    private void search() {
+        tSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                adapter.getFilter().filter(charSequence.toString());
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+            }
+        });
+    }
+
 }
